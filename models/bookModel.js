d5b61ae9457f91417a6e5b9f7586d13f2b19abac
noman-092 <numanansari078@gmail.com>
2024-06-07 19:06:14 +0530

    validition required

--- models/bookModel.js
+++ models/bookModel.js
@@ -11,6 +11,33 @@ const bookSchema =new mongoose.Schema({
         required:[true,"book name is required"],
         minLength:[5, "letter 5"],
         trim:true,
+    },
+    Price: {
+        type:Number,
+        default:0,
+        maxLength:9,
+    },
+
+    description:{
+        type:String,
+        required:[true,"description is required"],
+        minLength:[10],
+        trim:true,
+     
+    },
+    Isbn:{
+        type:String,
+        unique:true,
+        maxLength:13,
+        minLength:13,
+        trim:true,
+        uppercase:true,
+    },
+    Author:{
+        type:String,
+        required:[true,"description is required"],
+        minLength:4,
+        trim:true,
     }
 });
 
